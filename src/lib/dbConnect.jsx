c2a5e9e3c8af00b477148e5be41973ca0d7403d1
chandanyadavvasu <chandanyadavvasu@gmail.com>
2025-01-07 02:22:07 +0530

    first commit

---
+++ src/lib/dbConnect.jsx
@@ -0,0 +1,1 @@
+export const connectionStart = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/CarRenaTalForQutar?retryWrites=true&w=majority`;
